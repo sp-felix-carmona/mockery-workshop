1bd24d2a405aa6860b2fc1a603edcc32e7e986e7
Felix Carmona <felix.carmona@socialpoint.es>
2014-06-19 10:18:53 +0200

    exercise7 passive partial mock

--- src/Workshop/Tests/BillTest.php
+++ src/Workshop/Tests/BillTest.php
@@ -10,8 +10,12 @@ class BillTest extends \PHPUnit_Framework_TestCase
     public function testGetBillForUser()
     {
         $expensesData = array(50, 4);
-        $expenses = \Mockery::mock('\Workshop\Expenses[getBoughtProducts]')->shouldAllowMockingProtectedMethods();
+
+        // Traditional Partial Mock
+        $expenses = \Mockery::mock('\Workshop\Expenses[getBoughtProducts,logDiscount]')->shouldAllowMockingProtectedMethods();
+        
         $expenses->shouldReceive('getBoughtProducts')->andReturn($expensesData);
+        $expenses->shouldReceive('logDiscount')->andReturnUndefined();
         $bill = new Bill($expenses);
         $this->assertEquals(242, $bill->getBillForUser('felix'));
     }
@@ -19,8 +23,12 @@ class BillTest extends \PHPUnit_Framework_TestCase
     public function testGetBillForUserWithDiscount()
     {
         $expensesData = array(50, 4);
-        $expenses = \Mockery::mock('\Workshop\Expenses[getBoughtProducts]', array(10))->shouldAllowMockingProtectedMethods();
+
+        // Passive Partial Mock
+        $expenses = \Mockery::mock('\Workshop\Expenses', array(10))->shouldAllowMockingProtectedMethods()->makePartial();
+
         $expenses->shouldReceive('getBoughtProducts')->andReturn($expensesData);
+        $expenses->shouldReceive('logDiscount')->andReturnUndefined();
         $bill = new Bill($expenses, true);
         $this->assertEquals(217.8, $bill->getBillForUser('felix'));
     }
